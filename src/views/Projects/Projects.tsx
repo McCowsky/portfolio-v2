bb85ba4da09e498fce3d9e5999c1d415eb91353e
McCowsky <makowskimateusz@outlook.com>
2023-02-27 23:29:09 +0100

    projects carousel

--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -3,11 +3,28 @@ import { bestProjects } from "./bestProjects.json";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useEffect, useRef } from "react";
-
-gsap.registerPlugin(ScrollTrigger);
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper as SwiperClass } from "swiper/types";
+import { Virtual, Mousewheel, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/bundle";
+import { useState, useCallback } from "react";
+import { MdArrowForwardIos } from "react-icons/md";
+import { MdArrowBackIosNew } from "react-icons/md";
+import "./swiperStyles.css";
 const Projects: React.FC<{ projectsData: { [key: string]: any }[] }> = ({
   projectsData,
 }) => {
+  const [swiperRef, setSwiperRef] = useState<SwiperClass>();
+  const handlePrevious = useCallback(() => {
+    swiperRef?.slidePrev();
+  }, [swiperRef]);
+
+  const handleNext = useCallback(() => {
+    swiperRef?.slideNext();
+  }, [swiperRef]);
+
+  gsap.registerPlugin(ScrollTrigger);
   const animationRef = useRef(null);
   useEffect(() => {
     gsap.fromTo(
@@ -48,7 +65,7 @@ const Projects: React.FC<{ projectsData: { [key: string]: any }[] }> = ({
     <div className="w-full h-[calc(100vh-96px)] snap-center bg-[#0D002B]">
       <div className="h-auto w-[1920px] my-0 mx-auto px-10 ">
         <div
-          className="flex flex-col justify-center items-center gap-20 h-[calc(100vh-96px)]"
+          className=" h-[calc(100vh-96px)] my-0 mx-auto flex flex-col gap-20 justify-center items-center"
           ref={animationRef}
         >
           <div>
@@ -57,19 +74,48 @@ const Projects: React.FC<{ projectsData: { [key: string]: any }[] }> = ({
             </h3>
             <h2 className=" text-6xl text-center pt-3">Apps I've built</h2>
           </div>
-          <div className="grid grid-cols-3 gap-5">
-            {filteredProjects.map((project: { [key: string]: any }) => {
-              return (
-                <ProjectCard
-                  key={project.id}
-                  name={project.name}
-                  description={project.description}
-                  url={project.html_url}
-                  img_url={project.img_url}
-                  languages={project.languages_array}
-                />
-              );
-            })}
+          <div className="flex flex-col justify-center items-center gap-10">
+            <div className="w-[1580px] my-0 mx-auto flex justify-center items-center gap-10">
+              <button onClick={handlePrevious} className="text-7xl">
+                <MdArrowBackIosNew />
+              </button>
+              <Swiper
+                modules={[Virtual, Mousewheel, Pagination]}
+                spaceBetween={0}
+                slidesPerView={3}
+                slidesPerGroup={3}
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={setSwiperRef}
+                virtual
+                mousewheel
+                width={1433}
+                pagination={{
+                  el: ".swiper-custom-pagination",
+                  clickable: true,
+                  type: "bullets",
+                }}
+              >
+                {filteredProjects.map(
+                  (project: { [key: string]: any }, index: number) => {
+                    return (
+                      <SwiperSlide key={project.id} virtualIndex={index}>
+                        <ProjectCard
+                          name={project.name}
+                          description={project.description}
+                          url={project.html_url}
+                          img_url={project.img_url}
+                          languages={project.languages_array}
+                        />
+                      </SwiperSlide>
+                    );
+                  }
+                )}
+              </Swiper>
+              <button onClick={handleNext} className="text-7xl">
+                <MdArrowForwardIos />
+              </button>
+            </div>
+            <div className="swiper-custom-pagination flex justify-center text-yellow-400"></div>
           </div>
         </div>
       </div>
--- src/views/Projects/components/ProjectCard/ProjectCard.tsx
+++ src/views/Projects/components/ProjectCard/ProjectCard.tsx
@@ -1,3 +1,8 @@
+import { FiGithub } from "react-icons/Fi";
+import { FiMonitor } from "react-icons/Fi";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
 const ProjectCard: React.FC<{
   name: string;
   description: string;
@@ -6,24 +11,29 @@ const ProjectCard: React.FC<{
   languages: string[];
 }> = ({ name, description, url, img_url, languages }) => {
   return (
-    <div className="bg-[#14143A] w-[400px] py-10 px-10 rounded-2xl">
-      <img src={img_url} alt="" className="rounded-xl" />
-      <div className="flex pt-8 text-sm gap-5">
-        {languages.map((language) => {
-          return <h4 key={language}>{language}</h4>;
-        })}
+      <div className="bg-[#14143A] w-[400px] pt-10 pb-14 px-10 rounded-2xl">
+        <img src={img_url} alt="" className="rounded-xl" />
+        <div className="flex pt-8 text-sm gap-5">
+          {languages.map((language) => {
+            return <h4 key={language}>{language}</h4>;
+          })}
+        </div>
+        <h3 className="pt-4 text-orange-400 text-2xl">{name}</h3>
+        <p className="pt-8 text-lg">{description}</p>
+        <div className="upeercase flex justify-between pt-12 text-gray-300 text-sm">
+          <h4>
+            <a href="" target="_blank" className="flex items-center gap-2">
+              <FiMonitor /> Live demo
+            </a>
+          </h4>
+          <h4>
+            <a href={url} target="_blank" className="flex items-center gap-2">
+              <FiGithub />
+              Source code
+            </a>
+          </h4>
+        </div>
       </div>
-      <h3 className="pt-4 text-orange-400 text-2xl">{name}</h3>
-      <p className="pt-8 text-lg">{description}</p>
-      <div className="upeercase flex justify-between pt-10 text-gray-300 text-sm">
-        <h4>Live demo</h4>
-        <h4>
-          <a href={url} target="_blank">
-            Source code
-          </a>
-        </h4>
-      </div>
-    </div>
   );
 };
 
