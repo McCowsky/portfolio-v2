7e270e715bd91798817b2a475da556e1e12b29d2
McCowsky <makowskimateusz@outlook.com>
2023-03-09 05:31:47 +0100

    fixed project card image height

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,30 +1,31 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["roboto"],
-        oswald: ["oswald"],
+        roboto: ['roboto'],
+        oswald: ['oswald'],
       },
       colors: {
-        mainOrange: "#F1A208",
+        mainOrange: '#F1A208',
       },
     },
     screens: {
-      xl: { max: "2048px" },
+      xl: { max: '2048px' },
       // => @media (max-width: 1279px) { ... }
 
-      lg: { max: "1600px" },
+      lg: { max: '1600px' },
       // => @media (max-width: 1023px) { ... }
 
-      md: { max: "1366px" },
+      md: { max: '1366px' },
       // => @media (max-width: 767px) { ... }
 
-      sm: { max: "960px" },
+      sm: { max: '960px' },
       // => @media (max-width: 639px) { ... }
     },
   },
-  plugins: [require("tailwind-scrollbar-hide")],
-  darkMode: "class",
+  plugins: [require('tailwind-scrollbar-hide')],
+  darkMode: 'class',
 };
